7c44a8c76b4a9617532f743fa7ebb3f041dbf6d7
dcpai <huangxiaodong@dcpai.cn>
2017-08-16 11:24:54 +0800

    add vue-page===>vue

---
+++ src/components/main.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+    <v-navbar :tabs="tabs"></v-navbar>
+  </div>
+</template>
+
+<script>
+  import Navbar from './navbar.vue'
+
+  export default {
+    name: 'main',
+    props: {
+      tabs: {
+        type: Array
+      }
+    },
+    components: {
+      "v-navbar": Navbar
+    }
+  }
+</script>
+
+
+<style rel="stylesheet/scss" lang="scss">
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s ease;
+  }
+
+  .fade-enter, .fade-leave-active {
+    opacity: 0
+  }
+
+  .child-view {
+    position: absolute;
+    transition: all .5s cubic-bezier(.55, 0, .1, 1);
+  }
+
+  .slide-left-enter, .slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(30px, 0);
+    transform: translate(30px, 0);
+  }
+
+  .slide-left-leave-active, .slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-30px, 0);
+    transform: translate(-30px, 0);
+  }
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="nabbar-content">
+    <span class="nav-item" v-for="item in tabs">
+      <router-link :to="item.url">
+        <img :src="item.icon">
+        {{item.text}}
+      </router-link>
+    </span>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      tabs: {
+        type: Array
+      }
+    }
+  }
+</script>
+<style rel="stylesheet/scss" lang="scss">
+  .nabbar-content {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-top: 1px solid #2c3e50;
+    height: 50px;
+    line-height: 50px;
+  }
+
+  .nav-item {
+    display: inline-block;
+    width: 25%;
+    text-align: center;
+    float: left;
+    img {
+      width: 22px;
+      height: 22px;
+    }
+  }
+
+  .nav-item a {
+    color: #2c3e50;
+    text-decoration: none;
+  }
+
+  .nav-item a.router-link-exact-active {
+    color: #3399FF;
+  }
+</style>
